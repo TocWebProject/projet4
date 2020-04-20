f000e128c44f5bc69516db378210354c9f42d8c4
Yann Rioual <Toc@imac-de-yann.home>
2020-04-20 14:56:51 +0200

    Amélioration Model: Ajout Article Admin - Debug Login

--- src/Models/model.php
+++ src/Models/model.php
@@ -15,7 +15,6 @@ function lastThreeArticles()
 {
     $db = dbConnect();
     $lastThreePosts = $db -> query('SELECT id, title, content, DATE_FORMAT(creation_date, \'%d/%m/%Y\') AS day_post, DATE_FORMAT(creation_date, \'%Hh%imin%ss\') AS hour_post  FROM articles ORDER BY creation_date DESC LIMIT 0, 3');
-
     return $lastThreePosts;
 }
 
@@ -43,8 +42,8 @@ function extractContent($string, $max_length)
 }
 
 // Ajout d'un commentaire
-function addComment($postId, $author, $insertComment){
-    
+function addComment($postId, $author, $insertComment)
+{    
     $db = dbConnect();
     $ins = $db->prepare('INSERT INTO comments (articleid, author, comment, comment_date) VALUES (?, ?, ?, NOW())');
     $ins->execute(array($postId, $author, $insertComment));
@@ -54,6 +53,17 @@ function addComment($postId, $author, $insertComment){
     }
 
 }
+
+// Ajout d'un article
+function addPost($newTitle, $newContent)
+{
+    $db = dbConnect();
+    $ins = $db->prepare('INSERT INTO articles (id, title, content, creation_date) VALUES (null, ?, ?, NOW())');
+    $ins->execute(array($newTitle, $newContent));
+
+    return $ins;
+}
+
 
 // Récupération des commentaires d'un article.
 function getComments($postId)
@@ -84,34 +94,26 @@ function checkLogin($mail, $pwd)
         $query->execute(array($mail));
         $row=$query->fetch(PDO::FETCH_ASSOC);
 
-        // Vérification de la corespondance entre les informations entre la saisie de l'utilisateur et les informations de la base de donnée.
-        $isPasswordCorrect = password_verify($pwd, $row["pwdAdmin"]);
-        var_dump($mail);
-        var_dump($isPasswordCorrect);
-        var_dump($row['pwdAdmin']);
-        var_dump($pwd);
-
-        
+        // Si il n'y pas de résultat -> le mail admin n'existe pas.
         if (!$row) {
-            echo 'Mauvais identifiant ';
+            return false;
+        // Si il y a un résultat -> Vérification de la corespondance entre les informations entre la saisie de l'utilisateur et les informations de la base de donnée.
         } else {
+            $isPasswordCorrect = password_verify($pwd, $row["pwdAdmin"]);
+            // Si le mdp est correct. 
             if ($isPasswordCorrect) {
      
-                echo 'Vous êtes connecté !';
-                session_start();
                 $_SESSION['userid'] = $row['emailAdmin'];
-                header('location: ./index.php?action=dashboard');
+                return true;
 
             } else {
-                echo 'Mauvais identifiant ou mot de passe !';
+            // Si le mdp est incorrect.    
+                return false;
             }
         }
 
  }  
         
-
-
-
 
 // Connection à la base de données 
 function dbConnect()
